bcd223a6792fc37b818690851b209ada1065703d
Alexander Alekhin <sphrand@gmail.com>
2023-09-20 19:18:53 +0300

    Replaced standard error handling

--- Rust/smart_house/src/errors.rs
+++ Rust/smart_house/src/errors.rs
@@ -1,52 +1,13 @@
-use std::{
-    error::Error,
-    fmt::{Debug, Display},
-};
+use thiserror::Error;
 
-#[derive(Debug, PartialEq)]
-pub struct CheckStatusError {
-    pub(crate) kind: DeviceErrorKind,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum DeviceErrorKind {
+#[derive(Error, Debug)]
+pub enum DeviceError {
+    #[error("{0} device not found")]
     NotFound(String),
+    #[error("{0} not connection")]
     NotConnetion(String),
+    #[error("{0} is transmitting incorrect data")]
     InvalidData(String),
+    #[error("unknown error")]
     UnknownError,
-}
-
-impl CheckStatusError {
-    pub fn kind(&self) -> &DeviceErrorKind {
-        &self.kind
-    }
-}
-
-impl Display for CheckStatusError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.kind)
-    }
-}
-
-impl Display for DeviceErrorKind {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DeviceErrorKind::NotFound(device) => write!(f, "{device} not found"),
-            DeviceErrorKind::NotConnetion(device) => {
-                write!(f, "{device} not connection")
-            }
-            DeviceErrorKind::InvalidData(device) => {
-                write!(f, "{device} is transmitting incorrect data")
-            }
-            DeviceErrorKind::UnknownError => write!(f, "unknown error"),
-        }
-    }
-}
-
-impl Error for DeviceErrorKind {}
-
-impl Error for CheckStatusError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(&self.kind)
-    }
 }
